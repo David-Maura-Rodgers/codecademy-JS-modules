6f041b93661840c2eb32591167d9e2ad26617897
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2024-06-09 14:50:32 +0000

    import modules from dom function js to scret image and messages js files

--- secret-messages/secret-messages.js
+++ secret-messages/secret-messages.js
@@ -4,13 +4,13 @@ import { toggleHiddenElement, changeToFunkyColor } from '../modules/dom-function
 const buttonElement = document.getElementById('secret-button');
 const pElement = document.getElementById('secret-p');
 
-const toggleHiddenElement = (domElement) => {
-    if (domElement.style.display === 'none') {
-      domElement.style.display = 'block';
-    } else {
-      domElement.style.display = 'none';
-    }
-}
+// const toggleHiddenElement = (domElement) => {
+//     if (domElement.style.display === 'none') {
+//       domElement.style.display = 'block';
+//     } else {
+//       domElement.style.display = 'none';
+//     }
+// }
 
 buttonElement.addEventListener('click', () => {
   toggleHiddenElement(pElement);
